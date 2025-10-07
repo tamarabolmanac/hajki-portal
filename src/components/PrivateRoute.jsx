be0c318c9d0a5541b3c242bbbacfbda06cdeebc0
Tamara Burcak <tamarabolmanac@gmail.com>
2025-10-07 11:14:56 +0200

    Add redirection to login page for unauthenticated users

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -6,7 +6,7 @@ const PrivateRoute = ({ children }) => {
 
   return isLoggedIn ? children : children;
 
-  //<Navigate to="/login" replace />
+  <Navigate to="/login" replace />
 };
 
 export default PrivateRoute;
